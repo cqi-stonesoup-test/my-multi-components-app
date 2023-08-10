d3817eba3c8a6081ad89f253324a49b452ab6d32
Chenxiong Qi <qcxhome@gmail.com>
2023-08-10 15:08:06 +0800

    Use k8s core/v1 package

--- gocli/main.go
+++ gocli/main.go
@@ -4,10 +4,14 @@ import (
 	"fmt"
 
 	"k8s.io/apimachinery/pkg/types"
+	corev1 "k8s.io/api/core/v1"
 )
 
 func main() {
 	fmt.Println("go cli example program")
 	name := types.NamespacedName{Namespace: "default", Name: "image-controller"}
 	fmt.Println(name)
+
+	secret := corev1.Secret{}
+	fmt.Println("Secret:", secret)
 }
